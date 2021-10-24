21038f2ce32ecc8ca5909c0ee4e5b44d3ed69e86
Andrew Pylypiv <pylypiv10@gmail.com>
2021-10-24 11:35:32 +0300

    tabPagerAdded, viewBinding added

--- app/src/main/java/com/example/cocktails/MainActivity.kt
+++ app/src/main/java/com/example/cocktails/MainActivity.kt
@@ -3,13 +3,43 @@ package com.example.cocktails
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import com.example.cocktails.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : FragmentActivity() {
 
+    val numberOfTabs = 2
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setupViews()
+        setContentView(binding.root)
 
+    }
+
+    private fun setupViews() {
+        with(binding) {
+            val adapter = TabsPagerAdapter(supportFragmentManager, lifecycle, numberOfTabs)
+            tabsViewpager.adapter = adapter
+            // Link the TabLayout and the ViewPager2 together and Set Text & Icons
+            TabLayoutMediator(tabLayout, tabsViewpager) { tab, position ->
+                when (position) {
+                    0 -> {
+                        tab.text = "Ordinary drink"
+                    }
+                    1 -> {
+                        tab.text = "Cocktails"
+
+                    }
+                }
+                // Change color of the icons
+            }.attach()
+
+
+        }
     }
 
 }
